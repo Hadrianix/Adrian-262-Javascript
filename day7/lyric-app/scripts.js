99681391f3f4bf84e6c38b3b2e5e2c450cf08833
Hadrianix <33820513+Hadrianix@users.noreply.github.com>
2019-02-13 15:04:18 -0700

    finised scripts

--- day7/lyric-app/scripts.js
+++ day7/lyric-app/scripts.js
@@ -1,10 +1,45 @@
-var songURL = "https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime" + authorName + "/" + songTitle;
 
-fetch(songURL)
-    .then(function (response) {
-        if (!response.ok) {
-            throw Error(response.status);
-        }
-    })
+var submitButton = document.lyrics.submit
 
-console.log(songURL);
+submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+
+    var songArtist = document.getElementById('songArtist').value;
+    var songName = document.getElementById('songName').value;
+    var songURL = "https://api.lyrics.ovh/v1/" + songArtist + "/" + songName
+    var lyricArea = document.getElementById('lyricArea')
+
+    fetch(songURL)
+        .then(function (response) {
+            if (!response.ok) {
+                throw Error(response.status)
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            lyricArea.innerHTML = data.lyrics.replace(/\n/g, '<br>')
+
+        });
+
+
+
+
+})
+
+
+
+
+// fetch('http://jsonplaceholder.typicode.com/users')
+//     .then(function (response) {
+//         if (!response.ok) {
+//             throw Error(response.statusText);
+//         }
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     })
