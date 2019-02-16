49a8122a6ee8be99e5439d93885d1def59b6d008
Adrian Taylor <33820513+Hadrianix@users.noreply.github.com>
2019-02-15 23:51:47 -0700

    adding

--- day4/assignment4/scripts.js
+++ day4/assignment4/scripts.js
@@ -1,52 +1,54 @@
 
-// var quotes = [
-//     "heeere's Johnny! - The Shinning",
-//     "Is that $3,000 bounty on in cash or check? - Jaws",
-//     "Well hello, Mr. Fancy Pants - Army of Darkness",
-//     "Get away from her you bitch - Aliens",
-//     "Whatever you do, don't fall asleep. - A Nightmare on Elm Street"
-// ]
-
-
-
-
-
-// function newQuote() {
-//     var randomNumber = Math.floor(Math.random() * (quotes.length));
-//     document.getElementById('quotes').innerHTML = quotes[randomNumber];
-// }
 
 function createQuote () {
     var quotes = [
-        {quote: "Heeeere's Johnny!", movie: "The Shinning"}, 
-        {quote: "Is that $3,000 bounty on in cash or check?", movie: "Jaws"},
-        {quote: "Well hello, Mr. Fancy Pants - Army of Darkness", movie: "Army of Darkness"},
-        {quote: "Get away from her you bitch", movie: "Aliens"},
-        {quote: "What looked like morning was the beginning of endless night", movie: "The Exorcist"},
-        {quote: "You cannot run from this-it will follow you. It may lay dormant for years. Something may trigger it to become more active and it may over time reach out and communicate with you", movie: "Paranormal Activity"},
-        {quote: "They're coming from inside the house!", movie: "Black Christmas"},
-        {quote: "Hi, I'm Chucky. Wanna play?", movie: "Child's Play"},
-        {quote: "Do you know what a graveyard is? It's when the dead speaks!", movie: "Pet Sematary"},
-        {quote: "You know it's Halloween. I guess everyone's entitled to one good scare, huh?", movie: "Halloween"},
+        {quote: "Heeeere's Johnny!", movie: "The Shinning", img: "./images/the-shinning_1280x720.jpg"}, 
+        {quote: "Is that $3,000 bounty on in cash or check?", movie: "Jaws", img: "./images/jaws.jpg"},
+        {quote: "Well hello, Mr. Fancy Pants - Army of Darkness", movie: "Army of Darkness", img: "./images/army.jpg"},
+        {quote: "Get away from her you bitch", movie: "Aliens", img: "./images/aliens.jpg"},
+        {quote: "What looked like morning was the beginning of endless night", movie: "The Exorcist", img: "./images/exorcist.jpg"},
+        {quote: "You cannot run from this-it will follow you. It may lay dormant for years. Something may trigger it to become more active and it may over time reach out and communicate with you", movie: "Paranormal Activity", img: "./images/para.jpg"},
+        {quote: "They're coming from inside the house!", movie: "Black Christmas", img: "./images/Black-Christmas-Eye.jpg"},
+        {quote: "Hi, I'm Chucky. Wanna play?", movie: "Child's Play", img: "./images/childs.jpg"},
+        {quote: "Do you know what a graveyard is? It's when the dead speaks!", movie: "Pet Sematary", img: "./images/sematary.jpg"},
+        {quote: "You know it's Halloween. I guess everyone's entitled to one good scare, huh?", movie: "Halloween", img: "./images/halloween.jpg"},
     
     ];
 
-    var counter = 0;
 
-    function quoteShow() {
+
+
+
+    var counter = 0;
+        var images = [
+            './images/the-shinning_1280x720.jpg',
+            './images/jaws.jpg',
+            './images/army.jpg',
+            './images/aliens.jpg',
+            './images/exorcist.jpg',
+            './images/para.jpg',
+            './images/Black-Christmas-Eye.jpg',
+            './images/childs.jpg',
+            './images/sematary.jpg',
+            './images/halloween.jpg'
+        ];
+    
+        function quoteShow() {
         var theQuote = quotes[counter];
-        document.querySelector('#quotesBox').innerHTML = theQuote.quote;
+        document.querySelector('#quotesBox').innerHTML = theQuote.quote + '<span class="quote">';
         document.querySelector('#quotesBox, #author').innerHTML = theQuote.movie;
-        newTweet();
+        document.querySelector('#quotesBox, #img').innerHTML = theQuote.img;
+        // newTweet();
     }
 
     function theNextQuote() {
         if (counter < quotes.length -1) {
             counter++;
-        }else {
+        } else {
             counter = 0;
         }
         quoteShow();
+
     }
 
     function thePrevQuote() {
@@ -58,19 +60,33 @@ function createQuote () {
         quoteShow();
     }
 
-    function newTweet() {
-        var text = quotes[counter].quote.split(" ").join("%20") + "-" + quotes[counter].source.split(" ").join("%20");
-        var url = "https://twitter.com/intent/tweet?text=" + text;
-        document.querySelector("#tweet-span").innerHTML = '<a class="twitter-share-button" href="' + url + ' " data-size="large">Tweet</a>';
-        twttr.widgets.load();
-    }
 
     document.getElementById('next').addEventListener("click", theNextQuote);
     document.getElementById('prev').addEventListener("click", thePrevQuote);
     quoteShow();
-    var timer = setInterval(theNextQuote, 30000);
+    var time = setInterval(theNextQuote, 30000);
 }
 
 createQuote();
 
+// var button =  document.buttons.next
+
+// button.addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     document.getElementById("buttons").src="../assignment4/images/jaws.jpg";
+// })
 ///document.querySelector('#quotesBox').innerHTML = theQuote.quote + '<span class="quote-author"> &mdash; <a id="tweet" class="chosen-link" href="' + theQuote.link + '">' + theQuote.movie + '</a></span>';
+
+// var images = [
+//     './images/the-shinning_1280x720.jpg',
+//     './images/jaws.jpg',
+//     './images/army.jpg',
+//     './images/aliens.jpg',
+//     './images/exorcist.jpg',
+//     './images/para.jpg',
+//     './images/Black-Christmas-Eye.jpg',
+//     './images/childs.jpg',
+//     './images/sematary.jpg',
+//     './images/halloween.jpg'
+// ];
